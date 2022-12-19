3afe6d8206868e572814dcb504019d53ea1c2eca
Gerald <gerald@unterrainer.info>
2022-12-19 10:49:04 +0100

    update

--- src/main/java/info/unterrainer/htl/htlzeromq/Server.java
+++ src/main/java/info/unterrainer/htl/htlzeromq/Server.java
@@ -1,30 +1,30 @@
 package info.unterrainer.htl.htlzeromq;
 
-import lombok.extern.slf4j.Slf4j;
 import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Server {
 
 	public static void main(String[] args) throws Exception {
 		try (ZContext context = new ZContext()) {
-			//  Socket to talk to clients
+			// Socket to talk to clients
 			ZMQ.Socket socket = context.createSocket(SocketType.REP);
 			socket.bind("tcp://*:5555");
-
+			log.info("Starting Server.");
 			while (!Thread.currentThread().isInterrupted()) {
 				byte[] reply = socket.recv(0);
-				System.out.println(
-						"Received " + ": [" + new String(reply, ZMQ.CHARSET) + "]"
-				);
+				log.info("Received " + ": [" + new String(reply, ZMQ.CHARSET) + "]");
 				String response = "world";
 				socket.send(response.getBytes(ZMQ.CHARSET), 0);
-				Thread.sleep(1000); //  Do some 'work'
+				Thread.sleep(1); // Do some 'work'
 			}
 		} catch (InterruptedException e) {
-		Thread.currentThread().interrupt();
+			Thread.currentThread().interrupt();
 		}
+		log.info("Stopping Server.");
 	}
 }
