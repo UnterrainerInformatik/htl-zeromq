8ff6bfb4cbec6844f46c82a3748755f4355d8d7e
Gerald Unterrainer <gu_falcon@hotmail.com>
2022-12-27 18:18:35 +0100

    some updates

--- src/main/java/info/unterrainer/htl/htlzeromq/ChatServer.java
+++ src/main/java/info/unterrainer/htl/htlzeromq/ChatServer.java
@@ -10,7 +10,13 @@ import org.zeromq.ZMQ;
 import org.zeromq.ZMsg;
 
 import info.unterrainer.htl.htlzeromq.chatserver.Room;
+import lombok.extern.slf4j.Slf4j;
 
+/**
+ * This is a chat-server that also doubles as a greeting-server as in the first
+ * example. If only a single string is sent, then it works as a greeing-server.
+ */
+@Slf4j
 public class ChatServer {
 
 	private Map<String, Room> rooms = new HashMap<>();
@@ -18,11 +24,24 @@ public class ChatServer {
 	public void run() {
 		try (ZContext context = new ZContext()) {
 			// Socket to talk to clients
-			ZMQ.Socket socket = context.createSocket(SocketType.REP);
-			socket.bind("tcp://*:5555");
+			ZMQ.Socket inSocket = context.createSocket(SocketType.REP);
+			inSocket.bind("tcp://*:5556");
+			ZMQ.Socket pubSocket = context.createSocket(SocketType.PUB);
+			pubSocket.bind("tcp://*:5557");
 
 			while (!Thread.currentThread().isInterrupted()) {
-				ZMsg msg = ZMsg.recvMsg(socket);
+				ZMsg msg = ZMsg.recvMsg(inSocket);
+
+				if (msg.size() == 1) {
+					// Chat-Server-Mode.
+					String message = new String(msg.pop().getData(), StandardCharsets.UTF_8);
+					log.info("Received " + ": [" + message + "]");
+					String response = "world";
+					inSocket.send(response.getBytes(ZMQ.CHARSET), 0);
+					continue;
+				}
+
+				String type = new String(msg.pop().getData(), StandardCharsets.UTF_8);
 				String userId = new String(msg.pop().getData(), StandardCharsets.UTF_8);
 				String roomId = new String(msg.pop().getData(), StandardCharsets.UTF_8);
 				String message = new String(msg.pop().getData(), StandardCharsets.UTF_8);
@@ -30,7 +49,7 @@ public class ChatServer {
 				System.out.println(
 						"Message received " + ": [" + userId + "]" + " [" + roomId + "]" + " [" + message + "]");
 				String response = "ACK";
-				socket.send(response.getBytes(ZMQ.CHARSET), 0);
+				inSocket.send(response.getBytes(ZMQ.CHARSET), 0);
 			}
 		}
 	}
